0b308c0f48370ff499d683f2399e316d1373d951
Jeanny Elizabete <166667687+Elizabx@users.noreply.github.com>
2024-11-25 10:50:04 -0300

    Add files via upload

---
+++ Plataforma_Salas-ProjetoUnipe/BACKEND/auth/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SITE | PS</title>
+    <link rel="stylesheet" href="/freture/css/home.css" />
+</head>
+<body>
+    <h1>Seja Bem Vindo!</h1>
+    <h2>Faça sua reserva. Priorize sua privacidade e seu conforto!</h2>
+    <div class="box">
+        <a href="login.php">Login</a>
+        <a href="formulario.php">Cadastre-se</a>
+    </div>
+</body>
+</html>
